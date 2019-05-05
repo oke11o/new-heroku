a36b2df77597551282affcfc1db1ff7f8c52b9ce
Sergey Bevzenko <sbevzenko@ozon.ru>
2019-05-05 17:26:04 +0300

    Home page log

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,8 +11,10 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index()
+    public function index(LoggerInterface $logger)
     {
+        $logger->error('FIRST PAGE', ['context' => 'tmp']);
+
         return $this->render('home/index.html.twig', [
             'title' => 'World!!!',
         ]);
