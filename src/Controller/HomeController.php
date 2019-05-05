aa68236573cdd36beb721dd67a069355697e3285
Sergey Bevzenko <sbevzenko@ozon.ru>
2019-05-05 17:49:04 +0300

    Use database

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,8 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Tag;
+use App\Form\TagType;
+use App\Repository\TagRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
@@ -11,12 +16,27 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index(LoggerInterface $logger)
+    public function index(Request $request, LoggerInterface $logger, EntityManagerInterface $em, TagRepository $repo)
     {
-        $logger->error('FIRST PAGE', ['context' => 'tmp']);
+        $tag = new Tag();
+        $form = $this->createForm(TagType::class, $tag);
+
+        if ($request->isMethod('POST')) {
+            $form->handleRequest($request);
+            if ($form->isSubmitted() && $form->isValid()) {
+                $em->persist($tag);
+                $em->flush();
+
+                $logger->notice('Success create tag', ['tag' => $tag]);
+
+                return $this->redirectToRoute('home');
+            }
+        }
 
         return $this->render('home/index.html.twig', [
             'title' => 'World!!!',
+            'form' => $form->createView(),
+            'list' => $repo->findAll(),
         ]);
     }
 }
